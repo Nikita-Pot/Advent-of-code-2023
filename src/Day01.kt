98034164f4d2c408f7fc66dc1c07bc399742e054
NikitaPot <nikita.pot@student.hu.nl>
2023-12-01 10:25:49 +0100

    Day one, Part one complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+fun main()
+{
+    val input = readInput("inputDay01")
+    part1(input)
+    //part2(input).println()
+}
+fun part1(input: List<String>)
+{
+    var answer = 0
+     input.map { line ->
+                val firstDigit = line.firstOrNull { it.isDigit() } // get first number
+                val lastDigit = line.lastOrNull { it.isDigit() }   // get last number
+                //println("$firstDigit$lastDigit") //check output
+                var number = "$firstDigit$lastDigit".toIntOrNull()  // set first and last number to one int
+                if (number != null)
+                {
+                answer+= number // add all number to one answer
+                }
+             }
+println(answer) // when whole document is done give the answer
+}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+fun part2(input: List<String>): Int {
+    return input.size
 }
