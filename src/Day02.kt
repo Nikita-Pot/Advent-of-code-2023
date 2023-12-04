369506c0cf702f9ed470a0754e47f52504d3f0bb
NikitaPot <nikita.pot@student.hu.nl>
2023-12-04 20:31:04 +0100

    deel 1 van dag 4 done!

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,10 +1,6 @@
 //The Elf would first like to know which games would have been possible
 // if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
-val testinput= listOf("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" ,
-        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" ,
-        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" ,
-        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" ,
-        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
+
 fun main() {
     val input = readInput("inputDay02")
    val answer= part1(input)
---
+++ src/Day03.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val input = readInput("inputDay03")
+    part1(input)
+   part2(input)
+
+
+}
+private fun part1(input: List<String>){
+
+}
+
+private fun part2(input: List<String>){}
