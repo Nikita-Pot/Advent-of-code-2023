45e5fda6ede213ac6518a49ae5c5596240e6f762
NikitaPot <nikita.pot@student.hu.nl>
2023-12-05 10:18:19 +0100

    Day04 helemaal af!

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,12 +1,3 @@
-val testinput = listOf(
-    "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n",
-    "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n",
-    "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n",
-    "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n",
-    "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n",
-    "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
-)
-
 fun main() {
     val input = readInput("inputDay04")
     part1(input)
@@ -24,11 +15,43 @@ private fun part1(input: List<String>) {
             numbers.split(" ").filter { it.isNotBlank() }.mapNotNull { if (it.isNotBlank()) it.toIntOrNull() else null }
                 .filter { it in winningNumbersList }
         matchingCount += 1 shl (filteredNumbers.size - 1)
-        println("Winning Numbers: $winningNumbers")
-        println("All Numbers: $numbers")
-        println("Filtered Numbers: $filteredNumbers")
-        println("matching numbers count: $matchingCount")
+        // println("Winning Numbers: $winningNumbers")
+        // println("All Numbers: $numbers")
+        // println("Filtered Numbers: $filteredNumbers")
+        //println("matching numbers count: $matchingCount")
     }
 }
 
-private fun part2(input: List<String>) {}
+private fun part2(input: List<String>) {
+    val n = input.size
+    val copies = ArrayList<Int>(n)
+    var cardnumber = 0
+    var som = 0
+
+    for (i in 1..n) {
+        copies.add(1)
+    }
+
+    input.forEach { card ->
+        var j = 1
+        var numberOfCards = copies[cardnumber]
+        val winningNumbers = card.substringAfter(":").substringBefore("|").trim()
+        val numbers = card.substringAfter("|").trim()
+        val winningNumbersList = winningNumbers.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+        val filteredNumbers =
+            numbers.split(" ").filter { it.isNotBlank() }.mapNotNull { if (it.isNotBlank()) it.toIntOrNull() else null }
+                .filter { it in winningNumbersList }
+        // println("cardnumber: $cardnumber")
+        //println("number of card $numberOfCards")
+
+        while (j < filteredNumbers.size + 1) {
+            copies[cardnumber + j] += numberOfCards
+            j++
+        }
+        cardnumber++
+    }
+    for (allcopies in copies) {
+        som += allcopies
+    }
+    println("Answer part 2 geeft total copies: $som")
+}
