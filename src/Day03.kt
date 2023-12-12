115f66e06de7ad7d8748e4d12104cdd9812b702b
NikitaPot <nikita.pot@student.hu.nl>
2023-12-12 22:33:41 +0100

    Day 3 part 2 heeft nog fout in de * en partnumber check. want 644 van testinput is geen valid partnumber

--- src/Day03.kt
+++ src/Day03.kt
@@ -12,6 +12,7 @@ val testinput = listOf(
     "...\$.*....\n",
     ".664.598.."
 )
+
 fun main() {
     val input = readInput("inputDay03")
     part1(input)
@@ -82,16 +83,17 @@ fun isInBound(x: Int, y: Int, arrayLength: Int, lineLength: Int): Boolean {
     return y in 0 until arrayLength && x in 0 until lineLength
 }
 
-class PartNumber(val x: Int, val y: Int, val number: Int) {
+class PartNumber(val y: Int, val x: Int, val number: Int) {
     var length = number.toString().length
-//    private var key = intArrayOf(x)[y]
+
+    //    private var key = intArrayOf(x)[y]
 //    fun getNumber():Int
 //    {
 //        return number
 //    }
-fun getPair(): Pair<Int, Int> {
-    return Pair(x, y)
-}
+    fun getPair(): Pair<Int, Int> {
+        return Pair(y, x)
+    }
 }
 
 private fun part2(input: List<String>): Int {
@@ -101,82 +103,122 @@ private fun part2(input: List<String>): Int {
     val coordinatesOfDigits = mutableListOf<IntArray>()
     val charactersOfNumber = mutableListOf<Char>()
     var answer = 0
-    var x: Int
+    // var x: Int
     val partNumberList = mutableListOf<PartNumber>()
-    val validPartnumbers = mutableMapOf<Pair <Int ,Int>, MutableList<Int>>()
+    val validPartnumbers = mutableMapOf<Pair<Int, Int>, MutableList<Int>>()
     for (y in 0 until arrayLength) {
-        x = 0
+        var x = 0
 
-        while (isCharacterDigitWithBoundCheck(x, y, array)) {
-            coordinatesOfDigits.add(intArrayOf(x, y))
+        while (isCharacterDigitWithBoundCheck(y, x, array)) {
+            coordinatesOfDigits.add(intArrayOf(y, x))
             charactersOfNumber.add(array[y][x])
             x++
         }
         var index = 0
         val line = array[y]
         var partNumber = 0
+        x = 0
         while (index < line.length) {
-            //TODO fix waardes partnumbers daar zit fout in
+
             if (line[index].isDigit()) {
                 partNumber = line[index] - '0'
-
+                index++
                 while (line[index].isDigit()) {
-                    partNumber = partNumber*10 + (line[index] - '0')
+                    partNumber = partNumber * 10 + (line[index] - '0')
                     index++
                 }
             }
+            if (partNumber != 0 && !line[index].isDigit()) {
+                partNumberList.add(PartNumber(y, index, partNumber))
+                println("$y,$index,$partNumber")
+                partNumber = 0
+            }
             index++
-        }
-        if (partNumber != 0) {
-            partNumberList.add(PartNumber(x, y, partNumber))
-            println("$x,$y,$partNumber")
         }
     }
 
-        println("Time to check")
-        partNumberList.forEach { partNumber ->
-            val resultPair = isPartNumber2(coordinatesOfDigits, array, arrayLength, lineLength) ?:Pair(0,0)
-            if (resultPair!= null) {
-                val numberPair = partNumber.getPair()
-                println("coordinaten gear: $resultPair, coordinaten number : $numberPair")
-                val (x,y)=resultPair
-                val number = partNumber.number
-                validPartnumbers.getOrPut(Pair(x,y)) { mutableListOf() }.add(number)
-                coordinatesOfDigits.clear()
-                charactersOfNumber.clear()
-                println("add gear")
-            }
+    println("Time to check")
+    partNumberList.forEach { partNumber ->
+        println("partnumber to check: ${partNumber.number}, ${partNumber.getPair()}")
+        val resultPair = checkNeighbours2(partNumber.getPair(), arrayLength, lineLength, partNumber.length, array)
+        if (resultPair != Pair(0, 0)) {
+            val numberPair = partNumber.getPair()
+            println("coordinaten gear: $resultPair, coordinaten number : $numberPair, number: ${partNumber.number}")
+            val (x, y) = resultPair
+            val number = partNumber.number
+            validPartnumbers.getOrPut(Pair(y, x)) { mutableListOf() }.add(number)
+            coordinatesOfDigits.clear()
+            charactersOfNumber.clear()
+            println("add gear")
+        }
 
+    }
+    println("time for math")
+    validPartnumbers.forEach { (key, value) ->
+        if (value.size > 1) {
+            answer += value[0] * value[1]
+            println("${value[0]} * ${value[1]}")
+            println("new add")
         }
-        println("time for math")
-        validPartnumbers.forEach { (key, value) ->
-            if (value.size >1) {
-                answer+=value[0]*value[1]
-                println("${value[0]} * ${value[1]}")
-                println("new add")
-            }
-        }
+    }
     println("answer of part two: $answer")
     return answer
 
-    }
-
-fun isAGear(x: Int, y: Int, array: List<String>): Boolean {
-    val charToCheck = array[y][x]
-    return !Character.isDigit(charToCheck) && charToCheck == '*'
 }
 
-fun isPartNumber2(
-    coordinatesOfDigits: List<IntArray>, array: List<String>, arrayLength: Int, lineLength: Int
-): Pair<Int, Int>? {
+fun checkNeighbours2(
+    coordinates: Pair<Int, Int>, arrayLength: Int, lineLength: Int, partNumberLength: Int, array: List<String>
+): Pair<Int, Int> {
+    var (yNumber, xNumber) = coordinates
+    var result = Pair(0, 0)
+    val input = array[yNumber][xNumber]
+    var i = xNumber - partNumberLength - 1
+    var j = -1
+    val numberBoundCheck = partNumberLength + 2
+    println("start = $input")
+    if(yNumber-1 <0){j=0}
+    while (j <= 1) {
+        while (i < 0) {
+            i++
+        }
+        val newYNumber = yNumber + j
+        if (isInCustomBound(i, newYNumber, arrayLength, lineLength)) {
+            println("y waarde: $newYNumber")
 
-    val result = coordinatesOfDigits.firstOrNull {
-        checkNeighbours(it, arrayLength, lineLength) { isAGear(it[0], it[1], array) }
+            while (i <= numberBoundCheck) {
+                if (i in 1..<lineLength) {
+                    val charToCheck = array[newYNumber][i]
+                    //println(charToCheck)
+                    if (!Character.isDigit(charToCheck) && charToCheck == '*') {
+                        println("$yNumber,$i,$charToCheck")
+                        result = Pair(newYNumber, i)
+                    }
+                    i++
+                }
+                i++
+            }
+            j++
+            i=xNumber - partNumberLength - 1
+        }
+        else j++
+
+//            if (!isInBound(x, y, arrayLength, lineLength)) {
+//                break
+//            } else {
+//                val charToCheck = array[y][x]
+//                if (!Character.isDigit(charToCheck) && charToCheck == '*') {
+//                    println("$y,$x,$charToCheck")
+//                    result = Pair(y, x)
+//                    //return Pair(x, y)
+//                }
+//            }
     }
-    return result?.let { Pair(it[0], it[1]) }
+
+    return result
 }
-
-
+fun isInCustomBound(x: Int, y: Int, arrayLength: Int, lineLength: Int): Boolean {
+    return x in 0 until lineLength && y in 0 until arrayLength
+}
 
 
 
