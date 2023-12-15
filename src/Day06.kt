8764bfa2b50ddb5c59554cfe443bf124c0b5fe6a
NikitaPot <nikita.pot@student.hu.nl>
2023-12-15 11:30:45 +0100

    Day 6 part 2 done!

--- src/Day06.kt
+++ src/Day06.kt
@@ -2,9 +2,9 @@ fun main() {
     val testinput = readInput("Day06Test")
     val input = readInput("inputDay06")
     check(part1(testinput) == 288)
-    part1(input)
-    //check(part2(testinput))
-
+    println("part one: ${part1(input)}")
+    check(part2(testinput) == 71503)
+    println("part two: ${part2(input)}")
 }
 
 private fun part1(input: List<String>): Int {
@@ -19,7 +19,7 @@ private fun part1(input: List<String>): Int {
 
         for (j in 1..maxTime) {
             val maxDistance = j * (maxTime - j)
-            println("maxDistance: $maxDistance, maxTime = $maxTime, j:$j ")
+
             if (maxDistance > distance[i]) {
                 newanswer++
             }
@@ -27,7 +27,21 @@ private fun part1(input: List<String>): Int {
 
         answer *= newanswer
         newanswer = 0
-        println(answer)
+    }
+    return answer
+}
+
+private fun part2(input: List<String>): Int {
+    val whitespaceRegex = Regex("\\s+")
+    val time = input[0].substringAfter(':').trim().split(whitespaceRegex).joinToString("").toLong()
+    val distance = input[1].substringAfter(':').trim().split(whitespaceRegex).joinToString("").toLong()
+    var answer = 0
+    for (j in 14..time) {
+        val maxDistance = j * (time - j)
+
+        if (maxDistance > distance) {
+            answer++
+        }
     }
     return answer
 }
