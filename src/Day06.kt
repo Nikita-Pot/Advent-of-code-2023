94c5760f0d9471484e5493c36e69f202135a5aae
NikitaPot <nikita.pot@student.hu.nl>
2023-12-16 16:27:54 +0100

    Day 6 verbeterde versie :)

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,7 +3,7 @@ fun main() {
     val input = readInput("inputDay06")
     check(part1(testinput) == 288)
     println("part one: ${part1(input)}")
-    check(part2(testinput) == 71503)
+    check(part2(testinput) == 71503L)
     println("part two: ${part2(input)}")
 }
 
@@ -12,36 +12,54 @@ private fun part1(input: List<String>): Int {
     val time = input[0].substringAfter(':').trim().split(whitespaceRegex).map { it.toInt() }
     val distance = input[1].substringAfter(':').trim().split(whitespaceRegex).map { it.toInt() }
     var answer = 1
-    var newanswer = 0
 
     for (i in time.indices) {
+        var left = 0
+        var right = time[i]
         val maxTime = time[i]
+        val recordDistance = distance[i]
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+            val maxDistance = mid * (maxTime - mid)
 
-        for (j in 1..maxTime) {
-            val maxDistance = j * (maxTime - j)
-
-            if (maxDistance > distance[i]) {
-                newanswer++
+            if (maxDistance > recordDistance) {
+                right = mid - 1
+            } else {
+                left = mid + 1
             }
         }
-
-        answer *= newanswer
-        newanswer = 0
+        if (maxTime and 1 == 0) {
+          answer*= ((maxTime shr 1) - left) * 2 + 1
+        }
+        else {
+            answer*=  ((maxTime shr 1) - left) * 2 + 2
+        }
     }
+
     return answer
 }
 
-private fun part2(input: List<String>): Int {
+private fun part2(input: List<String>): Long {
     val whitespaceRegex = Regex("\\s+")
     val time = input[0].substringAfter(':').trim().split(whitespaceRegex).joinToString("").toLong()
     val distance = input[1].substringAfter(':').trim().split(whitespaceRegex).joinToString("").toLong()
-    var answer = 0
-    for (j in 14..time) {
-        val maxDistance = j * (time - j)
+    var left = 14L
+    var right = time
+
+    while (left <= right) {
+        val mid = left + (right - left) / 2
+        val maxDistance = mid * (time - mid)
 
         if (maxDistance > distance) {
-            answer++
+            right = mid - 1
+        } else {
+            left = mid + 1
         }
     }
-    return answer
+    if (time and 1L == 0L) {
+     return   ((time shr 1) - left) * 2 + 1
+    }
+    else {
+      return  ((time shr 1) - left) * 2 + 2
+    }
 }
