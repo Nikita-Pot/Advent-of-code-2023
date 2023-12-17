fadd5961b89788e0153641523c74fd1d65ca3638
NikitaPot <nikita.pot@student.hu.nl>
2023-12-17 15:45:22 +0100

    Day 11 code cleanup but still nog working with the input

--- src/Day11.kt
+++ src/Day11.kt
@@ -21,6 +21,7 @@ fun addEmptyLineAfterNonHash(input: List<String>): List<String> {
 
     return result
 }
+
 fun main() {
     val testinput = readInput("Day11Test")
     val input = readInput("inputDay11")
@@ -29,6 +30,7 @@ fun main() {
     //check(part2(testinput) == 2)
     //println("part two: ${part2(input)}")
 }
+
 private fun part1(input: List<String>): Long {
     val completeInput = addEmptyLineAfterNonHash(input)
     val length = completeInput.size
@@ -38,7 +40,7 @@ private fun part1(input: List<String>): Long {
     for (y in 0 until length) {
         x = 0
         completeInput.forEach { line ->
-            while (isCharactertWithBoundCheck(y,x,completeInput)) {
+            while (isCharactertWithBoundCheck(y, x, completeInput)) {
                 if (isAGalaxy(y, x, completeInput)) {
                     coordinatesOfGalaxy.add(intArrayOf(y, x))
                 }
@@ -47,14 +49,14 @@ private fun part1(input: List<String>): Long {
         }
     }
     println("check coordinates")
-    var distance=0L
+    var distance = 0L
     for (i in coordinatesOfGalaxy.indices) {
         val currentCoordinate = coordinatesOfGalaxy[i]
         // Loop door de rest van de coördinaten en bereken de afstand
         for (j in (i + 1) until coordinatesOfGalaxy.size) {
             val nextCoordinate = coordinatesOfGalaxy[j]
             distance += calcDistance(currentCoordinate, nextCoordinate).toLong()
-           // println("a = $currentCoordinate , b = $nextCoordinate, newdistance = $distance")
+            // println("a = $currentCoordinate , b = $nextCoordinate, newdistance = $distance")
         }
     }
 
@@ -66,17 +68,19 @@ private fun isAGalaxy(y: Int, x: Int, array: List<String>): Boolean {
     val charToCheck = array[y][x]
     return !Character.isDigit(charToCheck) && charToCheck == '#'
 }
+
 private fun isCharactertWithBoundCheck(y: Int, x: Int, array: List<String>): Boolean {
     return y in array.indices && x in 0 until array[y].length
 }
-private fun calcDistance(coorinatesA:IntArray,coorinatesB: IntArray):Int{
+
+private fun calcDistance(coorinatesA: IntArray, coorinatesB: IntArray): Int {
 
     val (xA, yA) = coorinatesA
     val (xB, yB) = coorinatesB
     val verschilX = abs(xB - xA)
     val verschilY = abs(yB - yA)
     val distance = verschilX + verschilY
-     return distance
+    return distance
 
 
 }
